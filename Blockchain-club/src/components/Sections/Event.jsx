fa6ef3310fe08f1477644dff6db7d654b6ebc563
ksuga <kadin.suga@gmail.com>
2024-09-10 21:16:46 -0500

    Calendar bug, where it requires Cors

--- Blockchain-club/src/components/Sections/Event.jsx
+++ Blockchain-club/src/components/Sections/Event.jsx
@@ -6,13 +6,13 @@ function Event() {
         <div className='flex justify-center my-3'>
             <h1 className='text-3xl'>Events</h1>
         </div>
-        <div className='h-[800px] sm:h-[600px]'>
+        {/* <div className='h-[800px] sm:h-[600px]'>
             <iframe src="https://calendar.google.com/calendar/embed?src=loyolablockchain%40gmail.com&ctz=America%2FChicago"
                     className="top-0 left-0 w-full h-full border-0"
                     frameBorder="0"
                     scrolling="no"
                     />
-        </div>
+        </div> */}
     </div>
   )
 }
