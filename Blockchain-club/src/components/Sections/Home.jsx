7e5df421ed1ac3a90115a88222ea55ec0b56d207
ksuga <kadin.suga@gmail.com>
2024-09-11 13:06:15 -0500

    Edit home page

--- Blockchain-club/src/components/Sections/Home.jsx
+++ Blockchain-club/src/components/Sections/Home.jsx
@@ -8,10 +8,13 @@ function Home() {
           <div className="relative w-full h-full">
           <img src={layout} className="w-full h-full object-cover" alt="background" />
           <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-[#53112318]">
+            <div className='bg-gray-800 opacity-80 p-5 mx-5 md:mx-10 '>
+
             <h1 className='text-6xl font-bold text-[#FFD700]'>Blockchain LUC</h1>
             <p className='text-xl text-white'>
               LOYOLA'S ORGANIZATION THAT EXPLORES THE CRYPTOCURRENCY AND BLOCKCHAIN ECOSYSTEM
             </p>
+            </div>
           </div>
         </div>
     </div>
