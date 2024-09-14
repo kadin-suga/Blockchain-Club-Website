6ca69afff52d4a3ebab62e6933fad7c86627854c
ksuga <kadin.suga@gmail.com>
2024-09-13 20:34:52 -0500

    Updates

--- Blockchain-club/src/libs/consts/TeamRoster.jsx
+++ Blockchain-club/src/libs/consts/TeamRoster.jsx
@@ -38,6 +38,12 @@ export const TEAM_ROSTER = [
         img: {profile}
     },
     {
+        position: "Events Director",
+        individual: "Muqeet Usman",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, inventore explicabo esse provident assumenda numquam corrupti ab consequatur quia ipsa adipisci iure fugit culpa debitis tempora deserunt perferendis aut officia.",
+        img: {profile}
+    },
+    {
         position: "CAN Representative",
         individual: "Natalie Martinez",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, inventore explicabo esse provident assumenda numquam corrupti ab consequatur quia ipsa adipisci iure fugit culpa debitis tempora deserunt perferendis aut officia.",
