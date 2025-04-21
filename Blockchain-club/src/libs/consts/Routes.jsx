641db1f2c4d3907e18e249d489ef360d16337feb
Josie Peterburs <120046052+JoPeterburs@users.noreply.github.com>
2025-04-21 13:01:50 -0500

    Update Routes.jsx

--- Blockchain-club/src/libs/consts/Routes.jsx
+++ Blockchain-club/src/libs/consts/Routes.jsx
@@ -1,3 +1,4 @@
+console.log("AnalyticsPage loaded")
 import AboutPage from "../../components/Pages/AboutPage";
 import ContactPage from "../../components/Pages/ContactPage";
 import EventPage from "../../components/Pages/EventPage";
